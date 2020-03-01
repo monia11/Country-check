d71e7bc06adfaf70bf08344bf7d116ce0e9d3c71
Monika Maleszka <maleszka.m93@gmail.com>
2020-03-01 15:58:34 +0100

    Updates

---
+++ precache-manifest.a2cea3431efdd7c4b51eca85363a084b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d41af94b633a543a2bcae20a8acbdcc",
+    "url": "/Country-check/index.html"
+  },
+  {
+    "revision": "1c0851c8ea03ebb57dcd",
+    "url": "/Country-check/static/css/main.a6ca0b64.chunk.css"
+  },
+  {
+    "revision": "d0f1effdea714cf7a1cf",
+    "url": "/Country-check/static/js/2.4803b41b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Country-check/static/js/2.4803b41b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0851c8ea03ebb57dcd",
+    "url": "/Country-check/static/js/main.8eca73bc.chunk.js"
+  },
+  {
+    "revision": "18e7e5bbf49e51330125",
+    "url": "/Country-check/static/js/runtime-main.de5de602.js"
+  }
+]);
